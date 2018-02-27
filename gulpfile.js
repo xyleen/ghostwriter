65feba957a7ccab90cba59a34e4747d6842c6a89
Konrad Księski <xylleen@gmail.com>
2018-02-27 23:38:00 +0100

    Add watcher for auto-compiling Sass styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
+
+gulp.task('watch', () => {
+    return gulp.watch('./assets/sass/**/*.scss', ['styles']);
+});
+
 gulp.task('styles', () => {
     return gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
